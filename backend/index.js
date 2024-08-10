38da87ab7b1eb4b631efb052219dc757ffc24ddd
AhmadAbulkhairr <ahmad.m.abulkhair@gmail.com>
2024-08-10 12:36:28 +0300

    done

--- backend/index.js
+++ backend/index.js
@@ -6,31 +6,38 @@ const messagesHandler = require("./controllers/message");
 const { pool } = require("./models/db");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
-//Socket
+app.use(cors());
+app.use(express.json());
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
+
+// Socket.IO server attached to the same HTTP server
 const { Server } = require("socket.io");
-const io = new Server(5000, { cors: { origin: "*" } });
+const io = new Server(server, { cors: { origin: "*" } });
 
 io.use(authSocket);
 
 const clients = {};
-//
+
 io.on("connection", (socket) => {
   console.log("connected");
   socket.use(socketDebug);
-  
+
   const user_id = socket.handshake.headers.user_id;
   clients[user_id] = { socket_id: socket.id, user_id };
- 
+
   messagesHandler(socket, io);
 
-  //Error handler
+  // Error handler
   socket.on("error", (error) => {
     socket.emit("error", { error: error.message });
   });
 
-  // for disconnect
+  // For disconnect
   socket.on("disconnect", () => {
     console.log(socket.id);
     for (const key in clients) {
@@ -42,17 +49,7 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(cors());
-app.use(express.json());
-
-const userRouter = require("./routes/users");
-const cartRouter = require("./routes/cart");
-const orderRouter = require("./routes/order");
-const reviewRouter = require("./routes/review");
-const roleRouter = require("./routes/roles");
-const restaurantRouter = require("./routes/restaurants");
-const ridersRouter = require("./routes/riders");
-
+// Your other routes and middlewares
 app.use("/carts", cartRouter);
 app.use("/orders", orderRouter);
 app.use("/reviews", reviewRouter);
@@ -60,25 +57,11 @@ app.use("/users", userRouter);
 app.use("/roles", roleRouter);
 app.use("/restaurants", restaurantRouter);
 app.use("/riders", ridersRouter);
-const itemRouter = require("./routes/item");
-
-const email = require('./routes/email');
+app.use("/items", itemRouter);
 app.use('/contact', email);
 
-
-const auth_socket = require("./middleware/auth_socket");
-const messageHandler = require("./controllers/message");
-
+// Stripe route
 app.use('/create-payment-intent', require("./routes/stripe"));
 
-
-app.use("/items", itemRouter);
-
 // Handles any other endpoints [unassigned - endpoints]
-
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
-
-app.listen(PORT, () => {
-  console.log(PORT);
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
